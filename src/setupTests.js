bfe8485164a015650c0bdeac09281349407aa23a
bhanukiran12 <86311373+bhanukiran12@users.noreply.github.com>
2025-02-07 14:03:49 +0000

    Pending changes exported from your codespace

---
+++ src/setupTests.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+
+// import '@testing-library/jest-dom'
+// import {configure} from '@testing-library/react'
+// import {configure as eConfigure} from 'enzyme'
+// import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
+
+// eConfigure({adapter: new Adapter()})
+
+/* eslint-disable */
+
+import '@testing-library/jest-dom'
+import {configure} from '@testing-library/react'
+import {configure as eConfigure} from 'enzyme'
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
+
+eConfigure({adapter: new Adapter()})
